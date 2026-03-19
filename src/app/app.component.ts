d9e066710c9eafc3dc4b220003adb590a2ba9a6f
Pablo Durán <pabloduranballesteros@gmail.com>
2026-03-19 18:09:39 +0100

    Modificacion de app para login y registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HabitTrack';
   sidebarOpen = false;
+  isAuthPage = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.updateAuthStatus(this.router.url);
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.updateAuthStatus(event.urlAfterRedirects);
+    });
+  }
+
+  private updateAuthStatus(url: string) {
+    const authRoutes = ['/sign-in', '/create-account'];
+    this.isAuthPage = authRoutes.some(route => url.includes(route));
+  }
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
